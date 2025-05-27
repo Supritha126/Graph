430313d53ececba2ebb4a1a50f7f2fac7fd457f1
Supritha D <suprithad76@gmail.com>
2025-05-27 21:59:13 +0530

    This is Scatterplot Graph.

---
+++ Scatter.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 1, 3, 5]
+plt.scatter(x, y)
+
+# Add labels and title
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.title("Simple Scatter Plot")
+plt.show()
